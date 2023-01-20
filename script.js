8def7c899c0bc48e255264f3861ad58d0a8e622b
Mert ULAS <h.mert.ulas@gmail.com>
2023-01-20 03:46:42 +0300

    Update script.js

--- script.js
+++ script.js
@@ -21,15 +21,6 @@ const sceneRect = {
 sceneCanvas.width = sceneRect.width;
 sceneCanvas.height = sceneRect.height;
 
-function getRandomColor() {
-    var letters = '0123456789ABCDEF';
-    var color = '#';
-    for (var i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-}
-
 class SeedsGraph {
     constructor (seedCount) {
         this.width = seedsRect.width;
