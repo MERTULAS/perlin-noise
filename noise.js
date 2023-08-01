d3d1c84c1c2b4505bca4afc25093823fe6aa06c8
MERTULAS <h.mert.ulas@gmail.com>
2023-08-02 02:24:09 +0300

    add new random points generator

--- noise.js
+++ noise.js
@@ -1,9 +1,59 @@
-function SeedGenerator (seedLength) {
-    return Array.from({length: seedLength}, () => Math.random());
+class Generator {
+    constructor(waveLength, amplitude, height) {
+        this.waveLength = waveLength;
+        this.amplitude = amplitude;
+        this.frequency = 1 / this.waveLength;
+        this.height = height;
+        this.INTERPOLATION = CosineInterpolation;
+    }
+
+    PseudoRandomNumberGenerator(numberLength) {
+        var M = 4294967296,
+        A = 1664525,
+        C = 1;
+        var Z = Math.floor(Math.random() * M);
+        
+        function rand(){
+            Z = (A * Z + C) % M;
+            return Z / M - 0.5;
+        }
+
+        const result = [];
+
+        let firstPoint = rand(), secondPoint = rand(), yPoint;
+        
+        
+        for (let xPoint = 0; xPoint < numberLength; xPoint++) {
+            if (xPoint % this.waveLength === 0) {
+                firstPoint = secondPoint;
+                secondPoint = rand();
+                yPoint = (this.height / 2) + firstPoint * this.amplitude;
+            }
+
+            else {
+                yPoint = (this.height / 2)  + this.INTERPOLATION(firstPoint, secondPoint, (xPoint % this.waveLength) / this.waveLength) * this.amplitude;
+            }
+            
+            result.push(yPoint);
+        }
+    
+        return result;
+    };
+
+    RandomSeedGenerator (seedLength) {
+        return Array.from({length: seedLength}, () => Math.random());
+    }
 }
+
 
 function LinearInterpolation(p1, p2, t) {
     return (1 - t) * p1 + t * p2;
+}
+
+function CosineInterpolation(p1, p2, t) {
+    let ft = t * Math.PI;
+    let f = (1-Math.cos(ft)) * .5;
+    return (1 - f) * p1 + f * p2;
 }
 
 function PerlinNoise (count, seeds, nOctaves) {
@@ -38,8 +88,9 @@ function FadeFunction (t) {
 }
 
 export {
-    SeedGenerator,
+    Generator,
     LinearInterpolation,
     PerlinNoise,
-    FadeFunction
+    FadeFunction,
+    CosineInterpolation
 }
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import { SeedGenerator, LinearInterpolation, PerlinNoise, FadeFunction } from '/noise.js';
+import { Generator, LinearInterpolation, PerlinNoise, FadeFunction, CosineInterpolation } from '/noise.js';
 
 // init seeds
 const seedsCanvas = document.getElementById('seeds');
@@ -36,7 +36,9 @@ class SeedsGraph {
         this.height = seedsRect.height;
         this.seeds = [];
         this.seedCount = seedCount;
-        this.seeds = SeedGenerator(this.seedCount);
+        
+        let generator = new Generator(50, 100, this.height);
+        this.seeds = generator.PseudoRandomNumberGenerator(this.width);
         this.LERPPoints = [];
         this.fadedPoints = [];
     }
@@ -47,30 +49,30 @@ class SeedsGraph {
 
     drawInterpolationPoints () {
         this.seeds.reduce((p1, p2) => {
-            for (let i = .1; i <= .9; i += .1) {
-                this.LERPPoints.push(LinearInterpolation(p1, p2, i));
+            for (let i = .1; i <= 1; i += .1) {
+                this.LERPPoints.push(CosineInterpolation(p1, p2, i));
             }
             return p2;
         });
 
-        this.#draw(scene, this.LERPPoints, true);
+        this.#draw(scene, this.LERPPoints, false);
     }
 
     drawFadedPoints () {
-        this.fadedPoints = this.LERPPoints.map(point => FadeFunction(point));
+        this.fadedPoints = this.LERPPoints.map(point => FadeFunction(point / 100) + this.height / 2);
 
         this.#draw(scene2, this.fadedPoints, false);
     }
 
     #draw(panel, points, pointsShow) {
-        panel.strokeStyle = `#${Math.floor(Math.random()*16777215).toString(16)}`;
-        panel.fillStyle = `#${Math.floor(Math.random()*16777215).toString(16)}`;
+        panel.strokeStyle = `red`;
+        panel.fillStyle = `red`;
         panel.beginPath();
-        panel.lineTo(0, (this.height >> 1) + ((Math.max(...points) * this.height) >> 1) - points[0] * this.height);
+        // panel.lineTo(0, (this.height >> 1) + ((Math.max(...points) * this.height) >> 1) - points[0] * this.height);
 
         let stepSize = this.width / (points.length - 1);
         points.forEach((noise, index) => {
-            panel.lineTo(stepSize * index, (this.height >> 1) + ((Math.max(...points) * this.height) >> 1) - noise * this.height);
+            panel.lineTo(Math.floor(stepSize * index), noise);
             if (pointsShow) {
                 scene.arc(stepSize * index, (this.height >> 1) + ((Math.max(...this.LERPPoints) * this.height) >> 1) - noise * this.height, 3, 0, Math.PI * 2);
             }
